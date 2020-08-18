d4a7dc1a7fd421c82f2704fb90671d26422bd61f
Yvonne Chen <yche@synopsys.com>
2020-08-18 03:41:53 +0200

    further fix for python3 support

--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -9,7 +9,7 @@ import caffe
 import numpy as np
 import yaml
 from fast_rcnn.config import cfg
-from generate_anchors import generate_anchors
+from .generate_anchors import generate_anchors
 from fast_rcnn.bbox_transform import bbox_transform_inv, clip_boxes
 from fast_rcnn.nms_wrapper import nms
 
--- tools/demo.py
+++ tools/demo.py
@@ -82,7 +82,7 @@ def demo(net, image_name):
     scores, boxes = im_detect(net, im)
     timer.toc()
     print('Detection took {:.3f}s for '
-           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+           '{:d} object proposals'.format(timer.total_time, boxes.shape[0]))
 
     # Visualize detections for each class
     CONF_THRESH = 0.8
